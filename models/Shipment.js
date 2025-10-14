7349a3c8291e1038c3ec4d19eb1aaa66bf31d677
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-14 13:27:12 -0400

    Update Shipment.js

--- models/Shipment.js
+++ models/Shipment.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const shipmentSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true }, // email itilizatè a
-    weight: { type: String, required: true },
-    items: { type: String, required: true },
-    tariff: { type: String, default: "" },
-    status: { type: String, default: "En attente" },
+    email: { type: String, required: true },       // email itilizatè
+    weight: { type: String, default: "" },         // pwa pa obligatwa pou pre-alerte
+    items: { type: String, required: true },       // deskripsyon atik yo
+    tariff: { type: String, default: "" },         // tarif si admin mete li
+    status: { type: String, default: "En attente" }, // statut: En attente / Confirmé / Livré
   },
   { timestamps: true }
 );
