e7a6fed6d48c83f030dc5669d7e2fb4c6688b5ed
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-15 11:45:40 -0400

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,6 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import Admin from "../models/Admin.js";
 
-// ------------------
-// Middleware pou itilizatè
-// ------------------
 export const authMiddleware = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
@@ -18,26 +14,6 @@ export const authMiddleware = async (req, res, next) => {
     next();
   } catch (err) {
     console.error("Erreur authMiddleware:", err);
-    res.status(401).json({ error: "Token invalid" });
-  }
-};
-
-// ------------------
-// Middleware pou admin
-// ------------------
-export const adminMiddleware = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
-    if (!token) return res.status(401).json({ error: "Token manke" });
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const admin = await Admin.findById(decoded.id);
-    if (!admin) return res.status(401).json({ error: "Admin pa egziste" });
-
-    req.admin = admin;
-    next();
-  } catch (err) {
-    console.error("Erreur adminMiddleware:", err);
     res.status(401).json({ error: "Token invalid" });
   }
 };
