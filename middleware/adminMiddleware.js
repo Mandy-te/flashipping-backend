dff8c2297ab44361aad7266b12a859829e291c6e
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-14 22:33:14 -0400

    Update adminMiddleware.js

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,6 +1,17 @@
+import jwt from "jsonwebtoken";
+
 export default function adminMiddleware(req, res, next) {
-  if (!req.user || req.user.role !== "admin") {
-    return res.status(403).json({ error: "Access denied: Admin only" });
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: "Pa gen token" });
+
+  const token = authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Pa gen token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.admin = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Token pa valab" });
   }
-  next();
 }
