211b5e8229aeaf30b69b4ee6bd3b40ae7b736b28
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-15 13:39:55 -0400

    Update adminMiddleware.js

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,19 +1,18 @@
 import jwt from "jsonwebtoken";
-import Admin from "../models/Admin.js";
 
-export const adminMiddleware = async (req, res, next) => {
+// Middleware admin
+export const adminMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: "Pa gen token" });
+
+  const token = authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Pa gen token" });
+
   try {
-    const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
-    if (!token) return res.status(401).json({ error: "Token manke" });
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const admin = await Admin.findById(decoded.id);
-    if (!admin) return res.status(401).json({ error: "Admin pa egziste" });
-
-    req.admin = admin;
+    req.admin = decoded;
     next();
   } catch (err) {
-    console.error("Erreur adminMiddleware:", err);
-    res.status(401).json({ error: "Token invalid" });
+    res.status(401).json({ error: "Token pa valab" });
   }
 };
