13b1da2a4eb137aaa1dcd5d15b8f19f056e6852a
Mandy-te <68794982+Mandy-te@users.noreply.github.com>
2025-10-14 22:22:39 -0400

    Create Admin.js

---
+++ models/Admin.js
@@ -0,0 +1,27 @@
+// /models/Admin.js
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const adminSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+// Hash modpas avan save
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Metòd pou verifye modpas
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
